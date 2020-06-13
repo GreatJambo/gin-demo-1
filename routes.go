0e211c9c791e86b7171c96b77aa24d4ad368f090
phy.xyz <1906612210@qq.com>
2020-06-13 11:44:16 +0800

    完成所有源代码

--- routes.go
+++ routes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
-	r.Use(middleware.CORSMiddleware(),middleware.RecoveryMiddleware())
+	r.Use(middleware.CORSMiddleware(), middleware.RecoveryMiddleware())
 	r.POST("/api/auth/register", controller.Register)
 	r.POST("/api/auth/login", controller.Login)
 	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
@@ -15,9 +15,18 @@ func CollectRoute(r *gin.Engine) *gin.Engine {
 	categoryRoutes := r.Group("/categories")
 	categoryController := controller.NewCategoryController()
 	categoryRoutes.POST("", categoryController.Create)
-	categoryRoutes.PUT("/:id",categoryController.Update)//替换
-	categoryRoutes.GET("/:id",categoryController.Show)
-	categoryRoutes.DELETE("/:id",categoryController.Delete)
+	categoryRoutes.PUT("/:id", categoryController.Update) //替换
+	categoryRoutes.GET("/:id", categoryController.Show)
+	categoryRoutes.DELETE("/:id", categoryController.Delete)
+
+	postRoutes := r.Group("/posts")
+	postRoutes.Use(middleware.AuthMiddleware())
+	postController := controller.NewPostController()
+	postRoutes.POST("", postController.Create)
+	postRoutes.PUT("/:id", postController.Update) //替换
+	postRoutes.GET("/:id", postController.Show)
+	postRoutes.DELETE("/:id", postController.Delete)
+	postRoutes.POST("/page/list", postController.PageList)
 
 	return r
 }
